3414c11c5b1a831c1cde487c2c37affdf9b9400e
Bozo Dragojevic <bozzo@digiverse.si>
2021-11-18 11:45:49 +0100

    clear trailing whitespace in test

--- Wrappers/Python/testWizium.py
+++ Wrappers/Python/testWizium.py
@@ -1,4 +1,4 @@
-# ############################################################################	
+# ############################################################################
 
 __license__ = \
 	"""This file is part of the Wizium distribution (https://github.com/jsgonsette/Wizium).
@@ -19,7 +19,7 @@ __license__ = \
 __author__ = "Jean-Sebatien Gonsette"
 __email__ = "jeansebastien.gonsette@gmail.com"
 
-# ############################################################################	
+# ############################################################################
 
 import os
 import re
@@ -28,7 +28,7 @@ import random
 import time
 from libWizium import Wizium
 
-# ############################################################################	
+# ############################################################################
 
 # Update those paths if needed !
 if platform.system()=='Linux':
@@ -44,7 +44,7 @@ DICO_PATH = './../../Dictionaries/Fr_Simple.txt'
 # ============================================================================
 def draw (wiz):
     """Draw the grid content, with a very simple formating
-        
+
     wiz     Wizium instance"""
 # ============================================================================
     lines = wiz.grid_read ()
@@ -55,7 +55,7 @@ def draw (wiz):
 # ============================================================================
 def set_grid_1 (wiz):
     """Set the grid skeleton with a pattern of black boxes
-        
+
     wiz     Wizium instance"""
 # ============================================================================
 
@@ -77,7 +77,7 @@ def set_grid_1 (wiz):
 # ============================================================================
 def set_grid_2 (wiz):
     """Set the grid as a rectangular area with a hole at the center
-    
+
     wiz     Wizium instance"""
 # ============================================================================
 
@@ -98,7 +98,7 @@ def set_grid_2 (wiz):
 # ============================================================================
 def load_dictionary (wiz, dico_path):
     """Load the dictionary content from a file
-        
+
     wiz         Wizium instance
     dico_path   Path to the dictionary to load
     """
@@ -110,7 +110,7 @@ def load_dictionary (wiz, dico_path):
 
     # Remove what is not a letter, if any
     words = [re.sub('[^a-zA-Z]+', '', s) for s in words]
-  
+
     # Load dictionary
     wiz.dic_clear ()
     n = wiz.dic_add_entries (words)
@@ -124,7 +124,7 @@ def load_dictionary (wiz, dico_path):
 # ============================================================================
 def solve (wiz, max_black=0, heuristic_level=0, seed=0):
     """Solve the grid
-        
+
     wiz             Wizium instance
     max_black       Max number of black cases to add (0 if not allowed)
     heuristic_level Heuristic level (0 if deactivated)
@@ -137,24 +137,24 @@ def solve (wiz, max_black=0, heuristic_level=0, seed=0):
     # Configure the solver
     wiz.solver_start (seed=seed, black_mode='DIAG', max_black=max_black, heuristic_level=heuristic_level)
     tstart = time.time ()
-    
+
     # Solve with steps of 500ms max, in order to draw the grid content evolution
     while True:
-        status = wiz.solver_step (max_time_ms=500) 
+        status = wiz.solver_step (max_time_ms=500)
 
         draw (wiz)
         print (status)
 
-        if status.fillRate == 100: 
+        if status.fillRate == 100:
             print ("SUCCESS !")
             break
-        if status.fillRate == 0: 
+        if status.fillRate == 0:
             print ("FAILED !")
             break
-    
+
     # Ensure to release grid content
     wiz.solver_stop ()
-    
+
     tend = time.time ()
     print ("Compute time: {:.01f}s".format (tend-tstart))
 
@@ -206,9 +206,9 @@ elif EXAMPLE == 3:
         'ANORDIRAS',
         'SERIERONS',
         'ENDURANTE',
-        'STEMASSES',]    
+        'STEMASSES',]
     wiz.dic_add_entries (words)
-    
+
     wiz.grid_set_size (9,9)
     solve (wiz, max_black=0, heuristic_level=0)
 
